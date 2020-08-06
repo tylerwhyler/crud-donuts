239b7156fca52d2264c649e77c5b6238761f4c8b
tylerwhyler <tyjiman@gmail.com>
2020-08-06 10:24:21 -0700

    have basic main page layout completed

--- donut-shop-react/src/home.js
+++ donut-shop-react/src/home.js
@@ -1,13 +1,39 @@
 import React from 'react';
 
+import mockdata from "./mockdata.json"
+
+import "./styles/home.css"
+
 function Home() {
+
+    const renderDonuts = mockdata.map(item => {
+        const {donutname, 
+               description, 
+               pictureurl, 
+               price, 
+               quantity, 
+               category} = item   
+        return (
+            <div className="donut-item-wrapper">
+                <div className="donut-item">
+                    <h2>{donutname}</h2>
+                    <p>{description}</p>
+                    <p>{price}</p>
+                </div>
+                <div className="image-wrapper">
+                    <img src={pictureurl} alt="Donut" />
+                </div>    
+            </div>
+        );
+    })
+    
   return (
     <div className="home-wrapper">
-        <p>
-          Hi react from home
-        </p>
+        {renderDonuts}
+        <p>Footer</p>
     </div>
   );
 }
 
 export default Home;
+
