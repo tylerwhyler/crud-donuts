e897dba87254c4b930f229ae36681748f3a94dbd
tylerwhyler <tyjiman@gmail.com>
2020-08-07 08:44:41 -0700

    added api get requests

--- donut-shop-react/src/home.js
+++ donut-shop-react/src/home.js
@@ -8,41 +8,44 @@ import "./styles/home.css";
 
 function Home() {
 
+    const [ donutItems, setDonutItem ] = useState([])
+
     const getData = () => {
         axios.get("http://localhost:5000/donuts")
         .then(res => {
-            console.log(res.data)
+            setDonutItem(res.data)
         })
     }
-    
+
     useEffect(() => {
         getData()
     }, [])
 
-    const renderDonuts = mockdata.map(item => {
-        const {donutname, 
+    const renderDonuts = donutItems.map(item => {
+        const {name, 
                description, 
-               pictureurl, 
+               picture_url, 
                price, 
                quantity, 
                category} = item   
         return (
             <div className="donut-item-wrapper">
                 <div className="donut-item">
-                    <h2>{donutname}</h2>
+                    <h2>{name}</h2>
                     <p>{description}</p>
                     <p>{price}</p>
                 </div>
                 <div className="image-wrapper">
-                    <img src={pictureurl} alt="Donut" />
+                    <img src={picture_url} alt="Donut" />
                 </div>    
             </div>
         );
     })
-    
+
   return (
     <div className="home-wrapper">
         {renderDonuts}
+        {console.log(donutItems)}
         <p>Footer</p>
     </div>
   );
